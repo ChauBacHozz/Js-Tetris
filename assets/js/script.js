0ac5b1bba91ecddaae1254bd43a5aeadcb88abc1
ChauBacHozz <phongndsaiyan@gmail.com>
2022-04-07 11:33:47 +0700

    complete game grid

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,11 +4,16 @@ var gr_height = gr.offsetHeight;
 
 console.log(gr_width + ' ' + gr_height) 
 var count = 0;
-for (let index = 0; index < (gr_width/30) * (gr_height/30); index++) {
-    count++;
-    var sub_gr = document.createElement('div');
-    sub_gr.className = 'sub-gr';
-    gr.appendChild(sub_gr);
-
+var pos = []
+for (let i = 0; i < gr_height/30 + 2; i++) {
+    let a = [];
+    for (let j = 0; j < gr_width/30; j++) {
+        count++;
+        var sub_gr = document.createElement('div');
+        sub_gr.className = 'sub-gr';
+        gr.appendChild(sub_gr);
+        a.push(j);
+    }
+    pos.push(a);
 }
-console.log(count);
+console.log(pos);
